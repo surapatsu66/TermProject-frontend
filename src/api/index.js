489551b854243dfa16f15dd40d1af57d0b8ce281
surapatsu66 <surapat_su66@live.rmutl.ac.th>
2024-03-08 15:58:52 +0700

    index

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 export async function login({ email, password }) {
-  return await fetch("/api/auth/login", {
+  return await fetch("http://api-user66033.se-rmutl.net/api/auth/login", {
     method: "POST",
     body: JSON.stringify({ email, password }),
     headers: { "Content-Type": "application/json" },
@@ -18,7 +18,7 @@ export async function login({ email, password }) {
 }
 
 export async function searchArtworks({ keyword }) {
-  return await fetch(`/api/homepage/getArtworks/${keyword}`, {
+  return await fetch(`http://api-user66033.se-rmutl.net/api/homepage/getArtworks/${keyword}`, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   })
